7eb44395516a62c2a9ecbf9ef327343b84254c67
Youdeoo <youdeoooo@gmail.com>
2020-12-04 02:49:13 +0800

    1

--- killPUBG.bat
+++
@@ -1,6 +0,0 @@
-%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
-taskkill /f /im ExecPubg.exe
-taskkill /f /im TslGame.exe
-taskkill /f /im TslGame_BE.exe
-taskkill /f /im iigw_server.exe
-pause
